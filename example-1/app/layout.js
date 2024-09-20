1ad2f8897b83902ce8a79c74aa613f3c2c775aa3
rapa-matias-sinis <matias.rapaport@sinis.com.ar>
2024-09-20 15:30:27 -0300

    Funciona todo, pero falta crear la documentacion

---
+++ example-1/app/layout.js
@@ -0,0 +1,12 @@
+
+import './../style.css';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" style={{height : "100vh"}}>
+      <body style={{height : "100vh"}}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ example-1/app/page.js
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
+export default function Home() {
+  return (
+    <Container style={{height : "100vh"}} className='h-100%'>
+      <Row>
+        <Col> TExto normal</Col>
+        <Col>
+          {[
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+            
+          ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+              This is a {variant} alert—check it out!
+            </Alert>))}
+        </Col>
+      </Row>
+      <Row>
+        <Col> <p className="alpha"> Este color </p></Col>
+        <Col> <p style={{color : "var(--bs-pink)"}}> Este color </p></Col>
+        <Col> <p className='fs-1'> Este color </p></Col>
+
+        <Col> 
+        <Button variant="primary">Primary</Button>{' '}
+      <Button variant="pink">Secondary</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
